e82de10949d503e1e119e32bb70805370c90c8a0
Tecnika <nika.koledova@yandex.ru>
2022-10-20 16:06:03 +0300

    Add files via upload

---
+++ javascript/start.js
@@ -0,0 +1,10 @@
+sample(1);
+function sample(information)//шаблон
+{
+    console.log(information);
+   let  content=document.body.getElementsByClassName('info');
+   let a =content.innerHTML;
+    // content.i
+console.log(content);
+console.log(a);
+};
